bfced58a11a2f414f93240686292ef66d2804dd4
tsengwenwen <msfn387908@gmail.com>
2024-04-15 12:18:03 +0800

    Initial Commit

---
+++ 0415/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0415
+//
+//  Created by TsengWen on 2024/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
